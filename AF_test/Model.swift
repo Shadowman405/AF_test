08368c4eb2758cdf28f25306415ee6bc89bf6e6e
m_mitin <maximus405@gmail.com>
2022-11-08 21:55:08 +0300

    having fun with Alamofire and MTG api

---
+++ AF_test/Model.swift
@@ -0,0 +1,85 @@
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let card = try? newJSONDecoder().decode(Card.self, from: jsonData)
+
+import Foundation
+
+// MARK: - CardElement
+struct CardElement: Codable {
+    let name: String
+    let cmc: Int
+    let colorIdentity: [String]
+    let type: String
+    let supertypes, types, subtypes: [String]
+    let rarity, cardSet, setName, text: String
+    let artist, number, layout, multiverseid: String
+    let imageURL: String
+    let foreignNames: [ForeignName]
+    let printings: [String]
+    let originalType: String
+    let legalities: [LegalityElement]
+    let id: String
+
+    enum CodingKeys: String, CodingKey {
+        case name, cmc, colorIdentity, type, supertypes, types, subtypes, rarity
+        case cardSet = "set"
+        case setName, text, artist, number, layout, multiverseid
+        case imageURL = "imageUrl"
+        case foreignNames, printings, originalType, legalities, id
+    }
+}
+
+// MARK: - ForeignName
+struct ForeignName: Codable {
+    let name, type: String
+    let flavor: JSONNull?
+    let imageURL: String
+    let language: String
+    let multiverseid: Int
+
+    enum CodingKeys: String, CodingKey {
+        case name, type, flavor
+        case imageURL = "imageUrl"
+        case language, multiverseid
+    }
+}
+
+// MARK: - LegalityElement
+struct LegalityElement: Codable {
+    let format: String
+    let legality: LegalityEnum
+}
+
+enum LegalityEnum: String, Codable {
+    case legal = "Legal"
+}
+
+typealias Card = [CardElement]
+
+// MARK: - Encode/decode helpers
+
+class JSONNull: Codable, Hashable {
+
+    public static func == (lhs: JSONNull, rhs: JSONNull) -> Bool {
+        return true
+    }
+
+    public var hashValue: Int {
+        return 0
+    }
+
+    public init() {}
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.singleValueContainer()
+        if !container.decodeNil() {
+            throw DecodingError.typeMismatch(JSONNull.self, DecodingError.Context(codingPath: decoder.codingPath, debugDescription: "Wrong type for JSONNull"))
+        }
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        try container.encodeNil()
+    }
+}
--- AF_test/ViewController.swift
+++ AF_test/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textLbl: UILabel!
+    let urlMTG = "https://api.magicthegathering.io/v1/cards?page=311"
+    var cards = [CardElement]()
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    func fetchCards() {
+        AF.request(urlMTG).validate().responseJSON { responseData in
+            switch responseData.result {
+            case .success(let value):
+                let json = JSON(value)
+                print("\(json["cards"][44])")
+                self.textLbl.text = "NAME: \(json["cards"][44]["name"])" + "MANA COST: \n\(json["cards"][44]["manaCost"])" +
+                "RARITY: \n\(json["cards"][44]["rarity"])"
+//                DispatchQueue.main.async {
+//                    self.textLbl.text = "\(value)"
+//                    let someCards = value
+//                    //   let card = try? newJSONDecoder().decode(Card.self, from: jsonData)
+//                    let card = try? JSONDecoder().decode(CardElement.self, from: value ?? Data())
+//                    print(card)
+//                }
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
     }
 
-
+    @IBAction func fetchPressed(_ sender: Any) {
+        fetchCards()
+    }
+    
 }
 
+ 
