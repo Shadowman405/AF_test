79229fcbbb135bbc56818ae03479c98863a5dcc3
m_mitin <maximus405@gmail.com>
2022-11-12 21:49:14 +0300

    add Image Manager to load images
    and add UIImage class to work with cache

---
+++ AF_test/CardImageView.swift
@@ -0,0 +1,19 @@
+//
+//  CardImageView.swift
+//  AF_test
+//
+//  Created by Maxim Mitin on 12.11.22.
+//
+
+import UIKit
+
+class CardImageView: UIImageView {
+    
+    func fetchImage(from url: String) {
+        guard let imageURL = URL(string: url) else {return}
+        //use from cache
+        
+        //load image
+        
+    }
+}
